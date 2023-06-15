7aee024def4095333af7d22e794022c714d69de2
마현우 <mhw35689402@gmail.com>
2023-06-15 12:07:24 +0900

    feat : 데이터에 따라서 상품 오브젝트가 페이지에 동적으로 나타나도록 하는 상품 리스트 추가

--- src/app/product/ProductList.jsx
+++ src/app/product/ProductList.jsx
@@ -2,16 +2,44 @@ import React from 'react';
 import GlobalStyle from '../../styles/global.style';
 import Header from '../../components/common/Header';
 import Footer from '../../components/common/Footer';
+import { styled } from 'styled-components';
+import ListObject from '../../components/common/ListObject';
+import data from '../../db/bdnsShopProductData.json';
 
 const ProductList = () => {
 	return (
 		<>
 			<GlobalStyle />
 			<Header/>
-			
+			<TitleDiv>SHOP</TitleDiv>
+			<ListContainer>
+				{data.map(object => (
+					<ListObject 
+						key={object.id}
+						imgUrl={object.imgUrl}
+						name={object.name}
+						price={object.price}
+					/>
+				))}
+			</ListContainer>
 			<Footer/>
 		</>
 	);
 };
+
+const ListContainer = styled.div`
+	width: 95%;
+	margin: 0 auto;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
+const TitleDiv = styled.div`
+	text-align: center;
+	font-size: 34px;
+	font-family: 'LatoRegular','Noto Sans KR';
+	font-weight: bold;
+	margin: 35px 0px 45px;
+`;
 
 export default ProductList;
